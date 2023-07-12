6017cccd21c3705e64bca000dc3f0df7f1cf0272
Mridulish_26 <mridulmathur2630@gmail.com>
2023-07-12 23:52:10 +0530

    s1

--- src/Notes/SwitchCase.java
+++ src/Notes/SwitchCase.java
@@ -1,7 +1,17 @@
 package Notes;
 
+import java.util.Scanner;
+
 public class SwitchCase {
     public static void main(String[] args) {
-        System.out.println("hey!");
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a Fruit: ");
+        String fruit = in.next();
+        switch (fruit) {
+            case "Mango" -> System.out.println("King");
+            case "Apple" -> System.out.println("!Doctor");
+            case "Banana" -> System.out.println("Tasty");
+            default -> System.out.println("Enter valid fruit");
+        }
     }
 }
