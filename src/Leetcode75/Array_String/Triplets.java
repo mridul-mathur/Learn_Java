f4709587942e26d910f6d5610474e06f1663d60c
Mridulish_26 <mridulmathur2630@gmail.com>
2023-08-09 03:36:41 +0530

    QQQ

---
+++ src/Leetcode75/Array_String/Triplets.java
@@ -0,0 +1,25 @@
+package Leetcode75.Array_String;
+
+public class Triplets {
+    public static void main(String[] args) {
+        int[] arr = {20, 26, 10, 5, 13, 100};
+        System.out.println(increasingTriplet(arr));
+    }
+
+    private static boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        int left = Integer.MAX_VALUE;
+        int mid = Integer.MAX_VALUE;
+
+        for (int num : nums) {
+            if (num <= left) {
+                left = num;
+            } else if (num <= mid) {
+                mid = num;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
